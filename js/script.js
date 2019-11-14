895bf57d2426805f18105886ce6d2ab6523e503b
Adrian Siwiak <amsiwiak98@gmail.com>
2019-11-14 12:27:03 +0100

    Add computerMove, playerMove and game result

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 var computerMove, randomNumber;
 let rundomNumber = Math.floor(Math.random() * 3 + 1);
 console.log('wylosowana liczba to ' + randomNumber);
-if (randomNumber == '1')
+let computerMove = 'nieznany ruch';
+
+if (randomNumber == '1') {
     computerMove = 'kamień';
-else if (randomNumber == '2')
+}
+else if (randomNumber == '2') {
     computerMove = 'papier';
-else if (randomNumber == '3')
+}
+else if (randomNumber == '3') {
     computerMove = 'nozyce';
+}
 
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
@@ -23,4 +28,36 @@ else if (playerInput == '2') {
 else if (playerInput == '3') {
     playerMove = 'nozyce';
 }
-printMessage('Twój ruch to: ' + playerMove);
+else {
+    playerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+
+
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+}
+if (computerMove == 'Papier' && playerMove == 'kamień') {
+    printMessage('Ty przegrywasz!');
+}
+if (computerMove == 'nozyczki' && playerMove == 'papier') {
+    printMessage('Ty przegrywasz!');
+}
+if (computerMove == 'papier' && playerMove == 'nozyczki') {
+    printMessage('Ty wygrywasz!');
+}
+if (computerMove == 'kamień' && playerMove == 'nozyczki') {
+    printMessage('Ty przegrywasz!');
+}
+if (computerMove == 'nozczki' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+}
+if (computerMove == 'nozczki' && playerMove =='nieznany ruch') {
+    printMessage(' Wartość spoza przedziału 1-3, wybierz jeszce raz! ');
+}
+if (computerMove == 'kamień' && playerMove =='nieznany ruch') {
+    printMessage(' Wartość spoza przedziału 1-3, wybierz jeszce raz! ');
+}
+if (computerMove == 'papier' && playerMove =='nieznany ruch') {
+    printMessage(' Wartość spoza przedziału 1-3, wybierz jeszce raz! ');
+}
